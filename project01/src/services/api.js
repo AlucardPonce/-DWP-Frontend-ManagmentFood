f750854c3a3296da0372bf6309e4afcbb13151bc
Dev Jack <poncealucard@gmail.com>
2025-03-10 13:52:50 -0600

    update rank
    crear las apis

--- project01/src/services/api.js
+++
@@ -1,5 +0,0 @@
-import api from './axiosConfig';
-
-export const getData = () => api.get('/endpoint');
-export const postData = (data) => api.post('/endpoint', data);
-// Añade más llamadas API según sea necesario
